0d849f3f33c0a8295814b280bbe1828fbfa95068
mnobile2495 <77944454+mnobile2495@users.noreply.github.com>
2021-10-03 22:01:00 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
+#include <iostream>
+
+int cubicSurfaceArea(int a);
+
 int main() {
-    std::cout << "Hello World!";
-    return 0;
+  std::cout << "Hello Professor Pisan! \n";
+
+std::cout << "The surface area of a cube with length 18 is: " << cubicSurfaceArea(18) << '\n';
+return 0;
+
+
 }
